e43ccefaa811b6f6809cbe02dc0f1287263e175f
professor josé de assis <josedeassisfilho@gmail.com>
2024-09-12 22:18:42 -0300

    preparação do ambiente de trabalho

---
+++ hello.js
@@ -0,0 +1,26 @@
+/**
+ * Hello Word
+ * @author Caio Lopes
+ */
+
+console.log("Hello Word")
+
+___________________________
+Node - Plataforma que permite a execução do JavaScript fora do navegador (browser)
+http://nodejs.org/ (baixar sempre a versão LTS)
+
+Comandos básicos:
+node -v
+
+Gerenciador de pacote para o node:
+npm está relacionado a pasta do Projeto ou seja os pacotes são instalados na raiz do projeto e não no sistema operacional
+
+
+npm -v (versão do npm)
+npm ls (listar pacotes instalados ls)
+
+npm init (iniciar um novo projeto JavaScript)
+
+__________package.json
+
+json (JavaScript Object Notation)
