36deaf72c32ecdccb4c17858ee0af46bac1943d8
professor josé de assis <josedeassisfilho@gmail.com>
2024-09-19 22:17:09 -0300

    Estudo Variaveis

---
+++ exercicios/academia.js
@@ -0,0 +1,26 @@
+/**
+* Ficha de um aluno de academia
+*@author Caio Lopes
+*/
+// variaveis 
+let nome
+let idade
+let peso
+let altura
+let vip
+
+console.clear()
+nome = "Caio Lopes"
+idade = 32
+peso  =122
+altura = 1.90
+vip = true
+
+// saida
+console.log("Ficha do Aluno")
+console.log("_______________________________")
+console.log(`Nome: ${nome}`)
+console.log(`idade: ${idade}`)
+console.log(`peso: ${peso}`)
+console.log(`altura: ${altura}`)
+console.log(`vip: ${vip}`)
